2d748612b13039584d6b5f06d6657b845d584b2d
ANUPAMAKUMARAN <anupamaa2024@gmail.com>
2025-09-24 16:39:04 +0530

    jhgjg

--- src/data/MacCarouselData.jsx
+++ src/data/MacCarouselData.jsx
@@ -1,106 +1,130 @@
 export const MacCarouselData = {
-  backgroundColor: "#fff",
-  title: "Get to know Mac.",
-  titleColor: "#000",
-  slides: [
-    {
-      caption: "Apple Intelligence and macOS",
-      captionColor: "#555",
-      title: "Easy to use. Easy to love.",
-      titleColor: "#000",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#f5f5f7",
-      
-      description:
-        "Mac is built with privacy and security at its core, giving you full control over your data."
-    },
-    {
-      caption: "Performance and Battery Life",
-      captionColor: "#fff",
-      title: "Go fast. Go far.",
-      titleColor: "#fff",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#000",
-      description:
-        "Mac is built with privacy and security at its core, giving you full control over your data."
-   
-    },
-    {
-      caption: "Mac and iPhone",
-      captionColor: "#111",
-      title: "Dream team.",
-      titleColor: "#000",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#e6f3ef",
-      description:
-        "Mac is built with privacy and security at its core, giving you full control over your data."
-   
-    },
-    {
-      caption: "Compatibility",
-      captionColor: "#111",
-      title: "Mac runs your favourite apps.",
-      titleColor: "#000",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#d6ebff",
-    },
-    {
-      caption: "Privacy and Security",
-      captionColor: "#fff",
-      title: "Your data. Your rules.",
-      titleColor: "#fff",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#e743c3",
-    },
-     {
-      caption: "Apple Intelligence and macOS",
-      captionColor: "#555",
-      title: "Easy to use. Easy to love.",
-      titleColor: "#000",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#f5f5f7",
-    },
-    {
-      caption: "Performance and Battery Life",
-      captionColor: "#fff",
-      title: "Go fast. Go far.",
-      titleColor: "#fff",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#000",
-    },
-    {
-      caption: "Mac and iPhone",
-      captionColor: "#111",
-      title: "Dream team.",
-      titleColor: "#000",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#e6f3ef",
-    },
-    {
-      caption: "Compatibility",
-      captionColor: "#111",
-      title: "Mac runs your favourite apps.",
-      titleColor: "#000",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#d6ebff",
-    },
-    {
-      caption: "Privacy and Security",
-      captionColor: "#fff",
-      title: "Your data. Your rules.",
-      titleColor: "#fff",
-      image:
-        "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
-      backgroundColor: "#e743c3",
-    },
-  ],
+    backgroundColor: "#fff",
+    title: "Get to know Mac.",
+    titleColor: "#000",
+    slides: [
+        {
+            caption: "Apple Intelligence and macOS",
+            captionColor: "#555",
+            title: "Easy to use. Easy to love.",
+            titleColor: "#000",
+            image:
+                "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
+            backgroundColor: "#f5f5f7",
+            pages: [
+                { description: "Mac is built with privacy and security at its core, giving you full control over your data." },
+                { descriptionColor: "#555" },
+                { img: "https://thumbs.dreamstime.com/b/earth-globe-family-hands-world-environment-day-concept-elements-image-furnished-nasa-172933016.jpg", }
+
+            ]
+
+
+        },
+        {
+            caption: "Performance and Battery Life",
+            captionColor: "#fff",
+            title: "Go fast. Go far.",
+            titleColor: "#fff",
+            image:
+                "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
+            backgroundColor: "#000",
+             pages: [
+                { description: "Mac is built with privacy and security at its core, giving you full control over your data." },
+                { descriptionColor: "#555" },
+                { img: "https://thumbs.dreamstime.com/b/earth-globe-family-hands-world-environment-day-concept-elements-image-furnished-nasa-172933016.jpg", }
+            ]
+
+        },
+        {
+            caption: "Mac and iPhone",
+            captionColor: "#111",
+            title: "Dream team.",
+            titleColor: "#000",
+            image:
+                "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
+            backgroundColor: "#e6f3ef",
+           pages: [
+                { description: "Mac is built with privacy and security at its core, giving you full control over your data." },
+                { descriptionColor: "#555" },
+                { img: "https://thumbs.dreamstime.com/b/earth-globe-family-hands-world-environment-day-concept-elements-image-furnished-nasa-172933016.jpg", }
+            ]
+
+        },
+        {
+            caption: "Compatibility",
+            captionColor: "#111",
+            title: "Mac runs your favourite apps.",
+            titleColor: "#000",
+            image:
+                "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
+            backgroundColor: "#d6ebff",
+             pages: [
+                { description: "Mac is built with privacy and security at its core, giving you full control over your data." },
+                { descriptionColor: "#555" },
+                { img: "https://thumbs.dreamstime.com/b/earth-globe-family-hands-world-environment-day-concept-elements-image-furnished-nasa-172933016.jpg", }
+            ]
+
+        },
+        {
+            caption: "Privacy and Security",
+            captionColor: "#fff",
+            title: "Your data. Your rules.",
+            titleColor: "#fff",
+            image:
+                "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
+            backgroundColor: "#e743c3",
+             pages: [
+                { description: "Mac is built with privacy and security at its core, giving you full control over your data." },
+                { descriptionColor: "#555" },
+                { img: "https://thumbs.dreamstime.com/b/earth-globe-family-hands-world-environment-day-concept-elements-image-furnished-nasa-172933016.jpg", }
+            ]
+
+        },
+        {
+            caption: "Apple Intelligence and macOS",
+            captionColor: "#555",
+            title: "Easy to use. Easy to love.",
+            titleColor: "#000",
+            image:
+                "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
+            backgroundColor: "#f5f5f7",
+             pages: [
+                { description: "Mac is built with privacy and security at its core, giving you full control over your data." },
+                { descriptionColor: "#555" },
+                { img: "https://thumbs.dreamstime.com/b/earth-globe-family-hands-world-environment-day-concept-elements-image-furnished-nasa-172933016.jpg", }
+            ]
+
+        },
+        {
+            caption: "Performance and Battery Life",
+            captionColor: "#fff",
+            title: "Go fast. Go far.",
+            titleColor: "#fff",
+            image:
+                "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
+            backgroundColor: "#000",
+             pages: [
+                { description: "Mac is built with privacy and security at its core, giving you full control over your data." },
+                { descriptionColor: "#555" },
+                { img: "https://thumbs.dreamstime.com/b/earth-globe-family-hands-world-environment-day-concept-elements-image-furnished-nasa-172933016.jpg", }
+            ]
+
+        },
+        {
+            caption: "Mac and iPhone",
+            captionColor: "#111",
+            title: "Dream team.",
+            titleColor: "#000",
+            image:
+                "https://img.freepik.com/premium-photo/light-bulb-with-pink-blue-light-bulb-itcolorful-3d-light-bulbs-white-background-new_432516-26613.jpg?w=360",
+            backgroundColor: "#e6f3ef",
+             pages: [
+                { description: "Mac is built with privacy and security at its core, giving you full control over your data." },
+                { descriptionColor: "#555" },
+                { img: "https://thumbs.dreamstime.com/b/earth-globe-family-hands-world-environment-day-concept-elements-image-furnished-nasa-172933016.jpg", }
+            ]
+
+        },
+       
+    ],
 };
