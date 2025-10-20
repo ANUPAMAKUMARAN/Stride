530f9603296a721393fbe8422acb54dec415494e
ANUPAMAKUMARAN <anupamaa2024@gmail.com>
2025-10-20 18:51:51 +0530

    jhkj

--- src/data/ImageCarouselData.jsx
+++ src/data/ImageCarouselData.jsx
@@ -47,6 +47,46 @@ export const ImageCarouselData = {
       text: "Proper communication maintained by DLIFE team...",
       descriptionOne: "Team handled every request with ",
       descriptionTwo: "Flawless execution from start to end."
+    },
+     {
+      id: 1,
+      img: "https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg",
+      name: "Prannoy HS",
+      text: "Structured, organized and pleasant to deal with....",
+      descriptionOne: "DLIFE made our home interior journey ",
+      descriptionTwo: "Their team ensured quality and timely "
+    },
+    {
+      id: 2,
+      img: "https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg",
+      name: "Mr. Suresh Chandran",
+      text: "Working with DLIFE was such a wonderful...",
+      descriptionOne: "They understood our taste perfectly.",
+      descriptionTwo: "Every designexceeded expectations."
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=800&q=80",
+      name: "Mr. Johnson Daniel",
+      text: "Dear DLIFE team, we are well pleased...",
+      descriptionOne: "Professional designers and workflow.",
+      descriptionTwo: "Highly recommend their service quality."
+    },
+    {
+      id: 4,
+      img: "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?w=800&q=80",
+      name: "Mr. Surendra N M & Family",
+      text: "We are extremely satisfied with the service...",
+      descriptionOne: "Excellent finishing and premium ",
+      descriptionTwo: "Project completed before the promised"
+    },
+    {
+      id: 5,
+      img: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?w=800&q=80",
+      name: "Mr. Kiran Nair & Family",
+      text: "Proper communication maintained by DLIFE team...",
+      descriptionOne: "Team handled every request with ",
+      descriptionTwo: "Flawless execution from start to end."
     }
   ]
 };
